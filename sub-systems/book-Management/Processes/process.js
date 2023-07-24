9c8de1001352def23b71fc26d5a3435c980e37c8
anirudh-nayak-172k <anirudh.nayak.172k@gmail.com>
2023-07-25 00:19:03 +0530

    createBook: process level design

--- sub-systems/book-Management/Processes/process.js
+++ sub-systems/book-Management/Processes/process.js
@@ -1,10 +1,48 @@
-const logger = require ('../../../shared/src/configurations/logger.configurations')
-
-
+const logger = require('../../../shared/src/configurations/logger.configurations');
+const db = require('../../../shared/src/models/index');
+const { Op } = require('sequelize');
 module.exports.bookManagementProcesses = {
-    coreProcess1: async (asd)=>{
-        logger.info(`This is the function argument : ${asd}`);
-
-        return asd
-    },
-}
+  createBook: async ({
+    ISBN,
+    bookTitle,
+    author,
+    subtitle,
+    genre,
+    yearOfPublication,
+    bookAvailabilityStatus,
+    originalNumberOfCopies,
+    numberOfCopiesLeft,
+  }) => {
+    try {
+      const book = await db.bookManagement.findAll({
+        attributes: ['isbn13', 'isbn10', 'title'],
+        where: { [Op.or]: { isbn13: ISBN, isbn10: ISBN } },
+        raw: true,
+      });
+      if (book?.length) {
+        return {
+          message: 'book already exists.',
+        };
+      } else {
+        const addBook = await db.bookManagement.create({
+          isbn13: ISBN.length == 13 ? ISBN : null,
+          isbn10: ISBN.length == 10 ? ISBN : null,
+          title: bookTitle,
+          subtitle: subtitle,
+          authors: author,
+          categories: genre,
+          publishedYear: yearOfPublication,
+          status: bookAvailabilityStatus,
+          originalNumberOfCopies: originalNumberOfCopies,
+          numberOfCopiesLeft: numberOfCopiesLeft,
+        });
+        const addBookResponse = addBook.toJSON();
+        return {
+          message: 'book added successfully',
+        };
+      }
+    } catch (error) {
+      throw error;
+    }
+  },
+};
